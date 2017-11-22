218ba56d7f9f05dfac3fba58e47d4edd45f273fc
Agustín Chaves <aguchaves@gmail.com>
2017-11-22 14:58:57 -0300

    [UPDATE] root login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,14 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-// import { TabsPage } from '../pages/tabs/tabs';
-// import { LoginPage } from '../pages/login/login';
-import { SelectTeamPage } from '../pages/select-team/select-team';
+import { TabsPage } from '../pages/tabs/tabs';
+import { LoginPage } from '../pages/login/login';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage: any = SelectTeamPage;
+  rootPage: any = LoginPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
